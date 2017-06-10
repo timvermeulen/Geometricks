bd67a7f6fcedb2e56bd027af0484990139196b81
Tim Vermeulen <tvermeulen@me.com>
2017-06-10 21:01:56 +0200

    Remove TODO

--- Geometricks/OneDimensional.swift
+++ Geometricks/OneDimensional.swift
@@ -1,5 +1,4 @@
 protocol OneDimensional: Observable, RawValueType {
-	// TODO: make Fraction an associated type
     func point(at fraction: RawValue) -> RawPoint<RawValue>?
     func fractionOfNearestPoint(to point: RawPoint<RawValue>) -> RawValue?
 }
